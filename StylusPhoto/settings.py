c7b5d1698d367dd60d6f75e550b6acfb21007415
Vári Róbert <mail.robertvari@gmail.com>
2021-02-27 12:05:35 +0100

    pages.urls included

--- StylusPhoto/settings.py
+++ StylusPhoto/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import home_view, contact_view, about_view
+
+urlpatterns = [
+    path('', home_view),
+    path('about/', about_view),
+    path('contact/', contact_view),
+]
