7c6a98f55ba287deff45efd9b78b20ac2bc569ce
Vári Róbert <mail.robertvari@gmail.com>
2021-02-27 12:30:53 +0100

    all pages with templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home_view, contact_view, about_view
+from .views import home_view, contact_view, about_view, gallery_view
 
 urlpatterns = [
     path('', home_view),
     path('about/', about_view),
     path('contact/', contact_view),
+    path('gallery/', gallery_view),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 
 def home_view(request):
-    return HttpResponse("Hello World")
+    return render(request, "home.html")
 
 
 def contact_view(request):
-    return HttpResponse("Contact page")
+    return render(request, "contact.html")
 
 
 def about_view(request):
-    return HttpResponse("About page")
+    return render(request, "about.html")
+
+
+def gallery_view(request):
+    return render(request, "gallery.html")
