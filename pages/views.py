732a63a960bea6695361c3154c64c2c190bbac5d
Vári Róbert <mail.robertvari@gmail.com>
2021-03-13 09:37:38 +0100

    Image generator

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
 
+def image_generator(photo_number, resolution=(800, 600)):
+    photo_links = []
+
+    for i in range(photo_number):
+        photo_links.append(f"https://source.unsplash.com/{resolution[0]}x{resolution[1]}/?nature{i},water{i}")
+
+    return photo_links
+
+
 def home_view(request):
     context = {
-        "names": ["Robert", "Csaba", "Kriszta", "Tamás"]
+        "title": "photographica",
+        "subtitle": "Ahol a fotó és a grafika találkozik",
+        "content": "Grafikus vagyok, és fotós. Kreatív képeimmel szeretném azt megmutatni, ami nem létezik (vagy mégis?) Célom, hogy az emberek megálljanak képeim előtt és elgondolkozzanak. Érezzenek valamit, csodálkozzanak, szeressék, gyűlöljék, de mindenképp érzéseket váltson ki. Ne egyszerűen csak elmenjenek előtte, továbblépjenek. És ha valaki visszatér, hogy újra megnézze, akkor már boldog vagyok.",
+        "photos": image_generator(5)
     }
 
     return render(request, "home.html", context)
@@ -18,4 +30,8 @@ def about_view(request):
 
 
 def gallery_view(request):
-    return render(request, "gallery.html")
+    context = {
+        "photos": image_generator(50)
+    }
+
+    return render(request, "gallery.html", context)
