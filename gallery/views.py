39692c578277436ca61defd2d96b75facbd82024
Vári Róbert <mail.robertvari@gmail.com>
2021-03-13 11:46:47 +0100

    category menu, gallery layout

--- gallery/views.py
+++ gallery/views.py
@@ -4,6 +4,7 @@ from utilities.mock_data import image_generator
 
 def gallery_view(request):
     context = {
+        "categories": ["Wedding", "Nature", "Sport"],
         "photos": image_generator(50)
     }
 
