55e174db963e7754629b820428cdc6b5eb1f764f
Vári Róbert <mail.robertvari@gmail.com>
2021-03-13 12:35:32 +0100

    photo details

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import gallery_view
+from .views import gallery_view, photo_details
 
 urlpatterns = [
     path('', gallery_view),
+    path('<str:slug>/', photo_details)
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -9,3 +9,14 @@ def gallery_view(request):
     }
 
     return render(request, "gallery.html", context)
+
+
+def photo_details(request, slug):
+    context = {
+        "photo": f"https://source.unsplash.com/1600x900/?nature{slug},water{slug}",
+        "title": "Ez itt a kép",
+        "subtitle": "2020 aug. 1.",
+        "content": "A kép leírása...",
+    }
+
+    return render(request, "photo_details.html", context)
