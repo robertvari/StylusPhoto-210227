46d0e4325db84d12eb79d16aa17c625374150956
Vári Róbert <mail.robertvari@gmail.com>
2021-03-14 12:31:22 +0100

    Photo replace image

--- StylusPhoto/urls.py
+++ StylusPhoto/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('gallery/', include('gallery.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="photos")
+    description = models.TextField(max_length=1000)
+
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+    slug = models.SlugField(max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        # before save if image replaced?
+        self.replace_image()
+
+        super(Photo, self).save(*args, **kwargs)
+
+        # after save resize image
+        self.resize_image()
+
+    def replace_image(self):
+        try:
+            photo = Photo.objects.get(id=self.id)
+            if photo.image.name != self.image.name:
+                photo.image.delete(save=False)
+        except:
+            pass
+
+    def resize_image(self):
+        pass
+
+    def __str__(self):
+        return self.title
