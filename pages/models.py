c366870bbb634637cecfeb03fa726e3b55807043
Vári Róbert <mail.robertvari@gmail.com>
2021-03-14 10:35:07 +0100

    Contact model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=200)
+    phone = models.TextField(max_length=200)
+    facebook = models.URLField(max_length=200, blank=True)
+    instagram = models.URLField(max_length=200, blank=True)
