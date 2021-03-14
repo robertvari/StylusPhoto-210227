a7273611b1f0616bfa78fe9a57823b4143a0bf25
Vári Róbert <mail.robertvari@gmail.com>
2021-03-14 11:01:50 +0100

    Contact model admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["email", "phone", "facebook", "instagram"]
+    search_fields = ["email", "phone"]
+
+
+admin.site.register(Contact, ContactAdmin)
--- pages/models.py
+++ pages/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Contact(models.Model):
     email = models.EmailField(max_length=200)
-    phone = models.TextField(max_length=200)
+    phone = models.CharField(max_length=200)
     facebook = models.URLField(max_length=200, blank=True)
     instagram = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.email
